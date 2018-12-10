e2eace5edfa4019e9286e020b26e9df739c410eb
Amund Froknestad <amund.froknestad@no.experis.com>
2018-12-10 16:16:22 +0100

    PositionRepository: Restructuring

--- src/main/java/api/Repositories/PositionRepository.java
+++ src/main/java/api/Repositories/PositionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 public interface PositionRepository extends CrudRepository<Position, Integer>{
+    //BASIC QUERIES
     @Query("SELECT p FROM Position p WHERE position_id = :id")
     Position getById(@Param("id") Integer id);
 
@@ -18,13 +19,6 @@ public interface PositionRepository extends CrudRepository<Position, Integer>{
 
     @Query("SELECT p FROM Position p WHERE company_name = :companyName")
     List<Position> getByCompanyName(@Param("companyName") String companyName);
-
-    @Query("SELECT p FROM Position p WHERE company_name = :companyName AND date_collected = :date")
-    List<Position> getByCompanyNameAndDate(@Param("companyName") String companyName, @Param("date") LocalDate date);
-
-    @Query("SELECT p FROM Position p WHERE date_collected = :date AND company_name = :companyName AND position_duration = :positionDuration AND position_name = :positionName AND no_of_positions = :noOfPositions AND field = :field")
-    Position exists(@Param("date") LocalDate date, @Param("companyName") String companyName, @Param("positionDuration") String positionDuration,
-                     @Param("positionName") String positionName, @Param("noOfPositions") Integer noOfPositions, @Param("field") String field);
 
     @Query("SELECT p FROM Position p WHERE field = :field")
     List<Position> getByField(@Param("field") String field);
@@ -35,12 +29,23 @@ public interface PositionRepository extends CrudRepository<Position, Integer>{
     @Query("SELECT p FROM Position p WHERE field = :field AND company_name = :companyName")
     List<Position> getByFieldAndCompanyName(@Param("field") String field, @Param("companyName") String companyName);
 
-    @Query("SELECT p.noOfPositions FROM Position p WHERE field = :field AND company_name = :companyName")
-    List<Integer> countByFieldAndCompanyName(@Param("field") String field, @Param("companyName") String companyName);
+    //QUERIES WITH DATE
+    @Query("SELECT p FROM Position p WHERE company_name = :companyName AND date_collected = :date")
+    List<Position> getByCompanyNameAndDate(@Param("companyName") String companyName, @Param("date") LocalDate date);
 
+    //BASIC COUNT QUERIES
     @Query("SELECT p.noOfPositions FROM Position p WHERE company_name = :companyName")
     List<Integer> countByCompanyName(@Param("companyName") String companyName);
 
+    @Query("SELECT p.noOfPositions FROM Position p WHERE field = :field AND company_name = :companyName")
+    List<Integer> countByFieldAndCompanyName(@Param("field") String field, @Param("companyName") String companyName);
+
+    //COUNT QUERIES WITH DATE
     @Query("SELECT p.noOfPositions FROM Position p WHERE date_collected = :date AND company_name = :companyName")
     List<Integer> countByCompanyNameAndDate(@Param("companyName") String companyName,@Param("date") LocalDate date);
+
+    //OTHER QUERIES
+    @Query("SELECT p FROM Position p WHERE date_collected = :date AND company_name = :companyName AND position_duration = :positionDuration AND position_name = :positionName AND no_of_positions = :noOfPositions AND field = :field")
+    Position exists(@Param("date") LocalDate date, @Param("companyName") String companyName, @Param("positionDuration") String positionDuration,
+                    @Param("positionName") String positionName, @Param("noOfPositions") Integer noOfPositions, @Param("field") String field);
 }
